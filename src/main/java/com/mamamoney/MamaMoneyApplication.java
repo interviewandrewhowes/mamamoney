e483fa328f249d1a043f440b0137d2952610be2f
Andrew Howes <andrew@viamedia.co.za>
2020-07-18 14:25:55 +0200

    Initial commit

---
+++ src/main/java/com/mamamoney/MamaMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.mamamoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamaMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamaMoneyApplication.class, args);
+	}
+
+}
